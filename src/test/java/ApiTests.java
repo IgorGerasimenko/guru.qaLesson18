723d7748941b74787d2466e40c76db7b7f7cefd9
IgorGerasimenko <58310275+IgorGerasimenko@users.noreply.github.com>
2021-08-03 01:07:54 +0400

    Переписал один тест

---
+++ src/test/java/ApiTests.java
@@ -0,0 +1,103 @@
+import org.junit.jupiter.api.Test;
+import pojo.AuthData;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.core.Is.is;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static specs.Specs.requestSpec;
+import static specs.Specs.responseSpec;
+
+public class ApiTests {
+
+    @Test
+    void createUser() {
+        given()
+                .spec(requestSpec)
+                .body("{\n" +
+                        "    \"name\": \"morpheus\",\n" +
+                        "    \"job\": \"leader\"\n" +
+                        "}")
+                .when()
+                .post("/api/users")
+                .then()
+                .statusCode(201)
+                .body("name", is("morpheus"))
+                .body("job", is("leader"));
+    }
+
+    @Test
+    void updateUser() {
+        given()
+                .spec(requestSpec)
+                .body("{\n" +
+                        "    \"name\": \"morpheus\",\n" +
+                        "    \"job\": \"kotlomoy\"\n" +
+                        "}")
+                .when()
+                .put("/users/2")
+                .then()
+                .spec(responseSpec)
+                .body("name", is("morpheus"))
+                .body("job", is("kotlomoy"));
+    }
+
+    @Test
+    void deleteUser() {
+        given()
+                .when()
+                .delete("/users/2")
+                .then()
+                .statusCode(204);
+    }
+
+    @Test
+    void checkLoginUnSuccessful() {
+        given()
+                .spec(requestSpec)
+                .body("{\n" +
+                        "    \"email\": \"sydney@fife\"\n" +
+                        "}")
+                .when()
+                .post("/register")
+                .then()
+                .statusCode(400)
+                .body("error", is("Missing password"));
+    }
+
+
+    @Test
+    void checkLoginSuccessful() {
+        given()
+                .spec(requestSpec)
+                .body("{\n" +
+                        "    \"email\": \"eve.holt@reqres.in\",\n" +
+                        "    \"password\": \"cityslicka\"\n" +
+                        "}")
+                .when()
+                .post("/register")
+                .then()
+                .spec(responseSpec)
+                .body("token", is("QpwL5tke4Pnpja7X4"));
+    }
+
+    @Test
+    void checkLoginSuccessfulWithPojo() {
+        AuthData data =
+                given()
+                        .spec(requestSpec)
+                        .body("{\n" +
+                                "    \"email\": \"eve.holt@reqres.in\",\n" +
+                                "    \"password\": \"cityslicka\"\n" +
+                                "}")
+                        .when()
+                        .post("/register")
+                        .then()
+                        .spec(responseSpec)
+                        .extract().as(AuthData.class);
+
+        assertEquals("QpwL5tke4Pnpja7X4", data.getToken());
+
+    }
+
+
+}
---
+++ src/test/java/pojo/AuthData.java
@@ -0,0 +1,20 @@
+package pojo;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class AuthData {
+
+    private Integer id;
+
+    public String getToken() {
+        return token;
+    }
+
+    private String token;
+    private String email;
+    private String password;
+
+}
